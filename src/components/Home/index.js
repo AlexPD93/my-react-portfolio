eab021af2aa92ad464f0df4566df506ff502cafc
Alex Perez-Davies <alexperezdavies@gmail.com>
2023-09-11 17:16:48 +0100

    change contact link to linkedin profile

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -59,7 +59,10 @@ const Home = () => {
             />
           </h1>
           <h2>Frontend Developer </h2>
-          <Link to="/contact" className="flat-button">
+          <Link
+            to="https://www.linkedin.com/in/aperezdavies/"
+            className="flat-button"
+          >
             CONTACT ME
           </Link>
         </div>
